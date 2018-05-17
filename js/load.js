9ded49dc9ddd945f87bd6a1afa03eb88bbbb0ff8
antarctic-wcg <wcgui@yahoo.com>
2018-05-17 15:04:29 +0800

    5.17

--- js/load.js
+++ js/load.js
@@ -4,8 +4,6 @@ var loaderState = function(game){
         game.load.setPreloadSprite(preloadSprite, 0);//第二个参数值为零意味着雪碧将水平裁剪，值1表示其垂
         game.load.image("orangeCar","assets/320c.png");
         game.load.image("redCar","assets/530h.png");
-
-
         game.load.image("wulingCar", "assets/wulh.png");
         game.load.image("floor", "assets/1540_990.png");
         game.load.image("building", "assets/building.png");
@@ -14,14 +12,14 @@ var loaderState = function(game){
         game.load.image("zhuangshi3", "assets/zs3.png");
         game.load.image("engine", "assets/yqzt.png");//引擎展台
         //按钮图片组
-        game.load.image("queren", "assets/queren.png");
-        game.load.image("quxiao", "assets/quxiao.png");
-        game.load.image("xuanzhuan", "assets/xuanzhuan.png");
-        game.load.image("ckBtn", "assets/ckBtn.png");
-        game.load.image("moveBtn", "assets/moveBtn.png");
-        game.load.image("chaichuBtn", "assets/chaichuBtn.png");
+        game.load.image("queren", "assets/queren.PNG");
+        game.load.image("quxiao", "assets/quxiao.PNG");
+        game.load.image("xuanzhuan", "assets/xuanzhuan.PNG");
+        game.load.image("ckBtn", "assets/ckBtn.PNG");
+        game.load.image("moveBtn", "assets/moveBtn.PNG");
+        game.load.image("chaichuBtn", "assets/chaichuBtn.PNG");
 
-        game.load.image("ceshi", "assets/3400_2210.JPG");
+        game.load.image("ceshi", "assets/3400_2210.jpg");
     }
     this.create = function(){
         game.state.start("home");
