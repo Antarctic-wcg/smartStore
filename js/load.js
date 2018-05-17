f02d461c0f7694dedc5bc5701fee64ceaf0771c0
antarctic-wcg <wcgui@yahoo.com>
2018-05-17 14:51:53 +0800

    5.17

--- js/load.js
+++ js/load.js
@@ -3,13 +3,13 @@ var loaderState = function(game){
         var preloadSprite = game.add.sprite(13, game.height/2, "loading");
         game.load.setPreloadSprite(preloadSprite, 0);//第二个参数值为零意味着雪碧将水平裁剪，值1表示其垂
         game.load.image("orangeCar","assets/320c.png");
-        game.load.image("redCar", "assets/530h.PNG");
-        game.load.image("wulingCar", "assets/wulh.PNG");
-        game.load.image("floor", "assets/1540_990.PNG");
-        game.load.image("building", "assets/building.PNG");
-        game.load.image("zhuangshi1", "assets/zs1.PNG");
-        game.load.image("zhuangshi2", "assets/zs2.PNG");
-        game.load.image("zhuangshi3", "assets/zs3.PNG");
+        game.load.image("redCar", "assets/530h.png");
+        game.load.image("wulingCar", "assets/wulh.png");
+        game.load.image("floor", "assets/1540_990.png");
+        game.load.image("building", "assets/building.png");
+        game.load.image("zhuangshi1", "assets/zs1.png");
+        game.load.image("zhuangshi2", "assets/zs2.png");
+        game.load.image("zhuangshi3", "assets/zs3.png");
         game.load.image("engine", "assets/yqzt.png");//引擎展台
         //按钮图片组
         game.load.image("queren", "assets/queren.png");
